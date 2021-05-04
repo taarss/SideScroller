253a27616ee2ff9113ec6039696b83d57d29d0be
Christian Villadsen <christianbjerrumvilladsen@hotmail.com>
2021-05-04 10:15:27 +0200

    Added basic file Structure

---
+++ model/loginRegister.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SideScroller.model
+{
+    public class loginRegister
+    {
+    }
+}
---
+++ views/gameView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SideScroller.views
+{
+    /// <summary>
+    /// Interaction logic for gameView.xaml
+    /// </summary>
+    public partial class gameView : UserControl
+    {
+        public gameView()
+        {
+            InitializeComponent();
+        }
+    }
+}
